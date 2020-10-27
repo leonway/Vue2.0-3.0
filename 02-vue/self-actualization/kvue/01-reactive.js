746d068dce3b0325acc8459564299855846fc315
reamey <17318077256@163.com>
2020-10-27 17:57:14 +0800

    第二课 补充部分作业

--- 02-vue/self-actualization/kvue/01-reactive.js
+++ 02-vue/self-actualization/kvue/01-reactive.js
@@ -1,3 +1,19 @@
+// 数组的响应式操作
+// 1.替换数组原型中的7个方法
+const originalProto = Array.prototype
+// 备份 修改备份
+const arrayProto = Object.create(originalProto);
+["push","pop","shift","unshift"].forEach(method=>{
+  arrayProto[method] = function(...data) {
+    // 原始操作
+    originalProto[method].apply(this,data)
+    // 追加操作:通知更新
+    console.log('数组执行'+method+"操作:"+data);
+  }
+})
+
+
+// 对象的响应式操作
 function defineReactive(obj,key,val) {
   observe(val)
   Object.defineProperty(obj,key,{
@@ -19,8 +35,19 @@ function observe(obj) {
   if(typeof obj !=='object'||obj===null){
     return
   }
-  //遍历obj所有key 做响应式处理
+  if(Array.isArray(obj)){
+    // 覆盖原型 替换7个数组操作
+    obj.__protp__ = arrayProto
+    // 对数组内部元素执行响应化
+    const keys = Object.keys(obj)
+    for(let i = 0; i<obj.length;i++){
+      observe(obj[i])
+    }
+  }else{
+    //遍历obj所有key 做响应式处理
   Object.keys(obj).forEach(key=>defineReactive(obj,key,obj[key]))
+  }
+  
 }
 
 function set(obj,key,val) {
@@ -32,7 +59,18 @@ const obj = {
   bar:'bar',
   baz:{
     ff:'ff'
-  }
+  },
+  arr:[
+    1,
+    {
+      foo:'foo',
+      array:[
+        1,2,{
+          foo:'foo'
+        }
+      ]
+    }
+  ]
 }
 
 observe(obj)
@@ -52,6 +90,7 @@ set(obj,'dong','dong')
 obj.dong
 obj.foo
 
+obj.arr[1]
 //array
 //push/pop/shift/unshift...使用 defineProperty拦截不到
 // 
